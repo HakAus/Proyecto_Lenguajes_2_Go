639a1749d1cfe00846d2ebe1c8d54e99b06aa5f6
Fernando Alvarez <ferolsen@hotmail.com>
2021-05-22 11:33:10 -0600

    Functional DSW
    Ya se puede usar el algoritmo DSW para un solo caso. Ajuste en la cantidad de nodos deseados en la parte inferior para generalizar.

--- main/main.go
+++ main/main.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	// "github.com/Xfighter32/Proyecto_Lenguajes_2_Go/randgen"
-	// "fmt"
-
-	"fmt"
+	//"fmt"
 
 	"github.com/Xfighter32/Proyecto_Lenguajes_2_Go/trees"
 )
 
 func main() {
 	// fmt.Println(randgen.GetRandomArray(17,200))
-	
-	avl := trees.AVLTree{}
-	var tree trees.ITree = &avl
 
-	tree.Insert(4)
-	tree.Insert(6)
+	//avl := trees.AVLTree{}
+	//var tree trees.ITree = &avl
+	bt := trees.BinaryTree{}
+	var tree trees.ITree = &bt
+
+	tree.Insert(10)
 	tree.Insert(5)
-	tree.Insert(7)
 	tree.Insert(20)
-	tree.Insert(13)
-	tree.Insert(21)
+	tree.Insert(7)
 	tree.Insert(15)
-	tree.Insert(17)
+	tree.Insert(30)
+	tree.Insert(25)
+	tree.Insert(40)
+	tree.Insert(23)
+	bt.BalanceDsw()
 	// tree.IRD()
-	fmt.Println("\n", avl.ToString(0))
+	//fmt.Println("\n", bt.PrintToRight())
+	//fmt.Println(bt.ToString(0))
 }
--- trees/binarytree.go
+++ trees/binarytree.go
@@ -3,10 +3,10 @@ package trees
 import "fmt"
 
 type BinaryTree struct {
-	key			   int
-	value          int
-	leftChildren   *BinaryTree
-	rightChildren  *BinaryTree
+	key           int
+	value         int
+	leftChildren  *BinaryTree
+	rightChildren *BinaryTree
 }
 
 // INTERFACE
@@ -14,11 +14,11 @@ type BinaryTree struct {
 func (tree *BinaryTree) Insert(keyToInsert int) int {
 	if tree.IsEmpty() {
 		*tree = NewBinaryTree(keyToInsert, 1)
-		return + 1
+		return +1
 	} else {
 		if keyToInsert == tree.key {
 			tree.value++
-			return + 1
+			return +1
 		} else if keyToInsert < tree.key {
 			return tree.leftChildren.Insert(keyToInsert) + 2
 		} else {
@@ -52,7 +52,7 @@ func (tree *BinaryTree) GetLeftChildren() ITree {
 	return tree.leftChildren
 }
 
-func (tree * BinaryTree) GetRightChildren() ITree {
+func (tree *BinaryTree) GetRightChildren() ITree {
 	return tree.rightChildren
 }
 
@@ -61,7 +61,7 @@ func (tree *BinaryTree) IRD() {
 }
 
 // LOCAL
-func NewBinaryTree(newKey int, valueToInsert int) (BinaryTree) {
+func NewBinaryTree(newKey int, valueToInsert int) BinaryTree {
 	newTree := BinaryTree{
 		key:           newKey,
 		value:         valueToInsert,
@@ -73,8 +73,118 @@ func NewBinaryTree(newKey int, valueToInsert int) (BinaryTree) {
 func Binary_IRD_Rec(tree *BinaryTree) {
 	if !tree.IsBranchless() {
 		Binary_IRD_Rec(tree.leftChildren)
-		fmt.Println("key: ",tree.key, "value: ", tree.value)
+		fmt.Println("key: ", tree.key, "value: ", tree.value)
 		Binary_IRD_Rec(tree.rightChildren)
 	}
 }
 
+func (root *BinaryTree) rotateLeft() {
+	var newRoot *BinaryTree = root.rightChildren
+	root.rightChildren = newRoot.leftChildren
+	var newLeft BinaryTree = *root
+	newRoot.leftChildren = &newLeft
+	*root = *newRoot
+	newRoot = nil
+}
+
+func (root *BinaryTree) rotateRight() {
+	var newRoot *BinaryTree = root.leftChildren
+	root.leftChildren = newRoot.rightChildren
+	var newRight BinaryTree = *root
+	newRoot.rightChildren = &newRight
+	*root = *newRoot
+	newRoot = nil
+}
+
+//Toma un arbol y lo convierte en una linaea rotando los nodos hacia la derecha
+func (root *BinaryTree) backboneTree() bool {
+	fmt.Println(root.key)
+	if root.IsEmpty() {
+
+		return false
+	}
+	if root.leftChildren.IsEmpty() {
+		return root.rightChildren.backboneTree()
+	} else {
+		root.rotateRight()
+		root.backboneTree()
+	}
+	return false
+}
+
+//Calcular cual sera la altura del arbol y repetir el proceso el numero de niveles completos
+//Se hace una rotacion inicial. Donde los nodos del nivel inferios son calculador y se rotan en la primera fase
+//Toma el backbone creado y lo convierte en un arbol rotando a la izquierda el resto de nodos impares
+
+func (root *BinaryTree) treeBackbone() {
+	//Altura deseada con log2(totalDeNodos).ceil()
+	desiredNodes := 2
+	//desiredNodes -= 1
+	fmt.Println("Primera parte de fila a arbol")
+	root.treeBackboneFersto(&desiredNodes)
+	fmt.Println("Segunda parte de fila a arbol")
+	root.treeBackboneSecando()
+}
+
+func (root *BinaryTree) treeBackboneFersto(currentNodes *int) bool {
+	if *currentNodes == 0 { //No es necesario verificar los nodos vacios. Porque la cantidad de nodos deseada evita ese tpo de errores.
+		return false
+	} else {
+		*currentNodes -= 1
+		root.rightChildren.rightChildren.treeBackboneFersto(currentNodes)
+		root.rotateLeft()
+	}
+	return false
+}
+
+func (root *BinaryTree) treeBackboneSecando() bool {
+	if root.rightChildren.IsEmpty() { //No se si sean necesarias las dos comparaciones. Para mantener la funcionalidad en casos donde no hay hijos derechos
+		return false
+	} else if root.rightChildren.rightChildren.IsEmpty() { //Se avanza de dos nodos a la vez para solamente alterar los impares
+		return false
+	} else {
+		fmt.Println("Secando", root.key)
+		root.rightChildren.rightChildren.treeBackboneSecando() //Puede quitarse el return?
+		fmt.Println("AfterSecando", root.key)
+		root.rotateLeft()
+	}
+	fmt.Println("Final", root.key)
+	return false
+}
+
+func (root *BinaryTree) BalanceDsw() {
+	fmt.Println("Start tree to backbone")
+	root.backboneTree()
+	root.PrintToRight()
+	fmt.Println("Start backbone to tree")
+	root.treeBackbone()
+	root.rotateLeft()
+}
+
+func (root *BinaryTree) PrintToRight() bool {
+	fmt.Println("ToRight")
+	if root.IsEmpty() {
+		return false
+	} else {
+		fmt.Println(root.key)
+		fmt.Println(root.leftChildren)
+		return root.rightChildren.PrintToRight()
+	}
+}
+
+func (avl *BinaryTree) ToString(count int) string {
+	var indent string;
+	count++
+	if !avl.IsEmpty() {
+		var buff string;
+		buff += "[Key: " + fmt.Sprintf("%d", avl.key)
+		buff += "| Value: " + fmt.Sprintf("%d",avl.value)
+		for i := 0; i < count; i++ {
+			indent += "\t"
+		}
+		buff += "\n" + indent + avl.leftChildren.ToString(count)
+		buff += "\n" + indent + avl.rightChildren.ToString(count)
+		return buff
+	}
+	return "Empty"
+}
